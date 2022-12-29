ede5c24a513359e559da01792627713cb1d4e46e
unknown <youssef.ojeil@hotmail.com>
2022-12-28 21:32:09 -0500

    deployed smart contract, create next app and install ethers and web3modal

--- hardhat-tutorial/contracts/LW3Punks.sol
+++ hardhat-tutorial/contracts/LW3Punks.sol
@@ -6,12 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-
 contract LW3Punks is ERC721Enumerable, Ownable {
-    using Strings for uint256
+    using Strings for uint256;
 
     // used for base URI + tokenId
-    string _baseTokenUri;
+    string _baseTokenURI = "";
 
     // mint price
     uint256 public _price = 0.01 ether;
@@ -25,7 +24,7 @@ contract LW3Punks is ERC721Enumerable, Ownable {
     // total number of tokenIds minted
     uint256 public tokenIds;
 
-    modifier onlyWhenNotPaused {
+    modifier onlyWhenNotPaused() {
         require(!_paused, "Contract is currently paused");
         _;
     }
@@ -35,60 +34,68 @@ contract LW3Punks is ERC721Enumerable, Ownable {
      * name in our case is `LW3Punks` and symbol is `LW3P`.
      * Constructor for LW3P takes in the baseURI to set _baseTokenURI for the collection.
      */
-    constructor (string memory baseURI) ERC721("LW3Punks", "LW3P") {
-        _baseTokenUri = baseURI;
+    constructor(string memory baseURI) ERC721("LW3Punks", "LW3P") {
+        _baseTokenURI = baseURI;
     }
 
     /**
-    * @dev mint allows an user to mint 1 NFT per transaction.
-    */
+     * @dev mint allows an user to mint 1 NFT per transaction.
+     */
     function mint() public payable onlyWhenNotPaused {
         require(tokenIds < maxTokenIds, "Collection has sold out!");
         require(msg.value >= _price, "Not enough ETH to mint");
-        tokenIds +=1;
+        tokenIds += 1;
         _safeMint(msg.sender, tokenIds);
     }
 
     /**
-    * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
-    * returned an empty string for the baseURI
-    */
+     * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
+     * returned an empty string for the baseURI
+     */
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
     /**
-    * @dev tokenURI overides the Openzeppelin's ERC721 implementation for tokenURI function
-    * This function returns the URI from where we can extract the metadata for a given tokenId
-    */
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: Token does not exist, can't return URI");
+     * @dev tokenURI overides the Openzeppelin's ERC721 implementation for tokenURI function
+     * This function returns the URI from where we can extract the metadata for a given tokenId
+     */
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: Token does not exist, can't return URI"
+        );
 
         string memory baseURI = _baseURI();
 
         // checks if the length of the baseURI is greater than 0, if it is, return the baseURI and attach
-        // the tokenId and `.json` to it so that it knows the location of the metadata json file for a given 
+        // the tokenId and `.json` to it so that it knows the location of the metadata json file for a given
         // tokenId stored on IPFS, If baseURI is empty return an empty string
-        return bytes(baseURI).Length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json"))
+                : "";
     }
 
     /**
-    * @dev setPaused makes the contract paused or unpaused
-    */
+     * @dev setPaused makes the contract paused or unpaused
+     */
     function setPaused(bool val) public onlyOwner {
         _paused = val;
     }
-    
+
     /**
-    * @dev withdraw sends all the ether in the contract 
-    * to the owner of the contract
-    */
+     * @dev withdraw sends all the ether in the contract
+     * to the owner of the contract
+     */
     function withdraw() public onlyOwner {
         address _owner = owner();
         uint256 amount = address(this).balance;
         (bool sent, ) = _owner.call{value: amount}("");
         require(sent, "failed to send Ether");
-    }   
+    }
 
     // Function to receive Ether. msg.data must be empty
     receive() external payable {}
